3c959b7681d2ace6479cd161eb761584be7adcbb
WataNekko <88185666+WataNekko@users.noreply.github.com>
2021-11-27 21:18:54 +0700

    Merge from main branch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
+import './App.css';
+import React, { Fragment } from "react";
+
+//components
+import Profile from './components/Profile';
+import EditCalendar from './components/EditCalendar';
 import Volunteers from "./components/Volunteers";
 import Profile2 from "./components/Profile2";
 
 function App() {
   return (
-    <div className="App">
-      <main className="content">
-        <Volunteers />
+    <Fragment>
+      <div className="container">
+        <p>Profile</p>
+        <Profile />
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <br></br>
+        <hr></hr>
+        <p>Edit Calendar</p>
+        <EditCalendar />
+        {/* <Volunteers /> */}
         {/* <Profile2 /> */}
-      </main>
-    </div>
+      </div>
+
+    </Fragment>
   );
 }
-
 export default App;
