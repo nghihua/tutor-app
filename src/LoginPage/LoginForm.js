13b8f0d810247398837a5bc251d0e021d2d0c5f4
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2021-12-17 23:11:30 +0700

    quangae
    update  indent

--- src/LoginPage/LoginForm.js
+++ src/LoginPage/LoginForm.js
@@ -2,45 +2,45 @@ import { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 // const LoginForm = ({Login, error, changePage}) => {
 const LoginForm = () => {
-    const [details, setDetails] = useState({ name: "", email: "", password: "" });
-    // const [error, setError] = useState(false) // set something if login not correct
+  const [details, setDetails] = useState({ name: "", email: "", password: "" });
+  // const [error, setError] = useState(false) // set something if login not correct
 
-    const history = useHistory();
-    const submitHandle = (event) => {
-        // Login(details);
-        // fetch data to the server ???
-        // check if success then
-        history.push("/volunteer");
-        //else => error
-    };
+  const history = useHistory();
+  const submitHandle = (event) => {
+    // Login(details);
+    // fetch data to the server ???
+    // check if success then
+    history.push("/volunteer");
+    //else => error
+  };
 
-    return (
-        <div>
-            <div className='login'>
-                <form onSubmit={submitHandle}>
-                    <h2>Login</h2>
-                    {/* {(error) ? (<div className= "error"><h2>{error} </h2></div>) : ""} */}
+  return (
+    <div>
+      <div className='login'>
+        <form onSubmit={submitHandle}>
+          <h2>Login</h2>
+          {/* {(error) ? (<div className= "error"><h2>{error} </h2></div>) : ""} */}
 
-                    <label htmlFor="email">Email: </label>
-                    <input type="email" name="email" id="email" onChange={(event) => setDetails({ ...details, email: event.target.value })} value={details.email} />
+          <label htmlFor="email">Email: </label>
+          <input type="email" name="email" id="email" onChange={(event) => setDetails({ ...details, email: event.target.value })} value={details.email} />
 
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" name="password" id="password" onChange={(event) => setDetails({ ...details, password: event.target.value })} value={details.password} />
+          <label htmlFor="password">Password: </label>
+          <input type="password" name="password" id="password" onChange={(event) => setDetails({ ...details, password: event.target.value })} value={details.password} />
 
 
-                    <div className='center'>
-                        <button type="submit" value="submit">Submit</button>
-                    </div>
-                </form>
-                <div className='center'>
-                    <Link to="/register">Register</Link>
-                </div>
-            </div>
-            <footer>
-                <p className="center">@tutor2021</p>
-            </footer>
+          <div className='center'>
+            <button type="submit" value="submit">Submit</button>
+          </div>
+        </form>
+        <div className='center'>
+          <Link to="/register">Register</Link>
         </div>
-    );
+      </div>
+      <footer>
+        <p className="center">@tutor2021</p>
+      </footer>
+    </div>
+  );
 }
 
 export default LoginForm;
