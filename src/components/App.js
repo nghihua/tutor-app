1429749aa509284c2a737ba996510335b1f7135c
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2021-12-17 23:38:54 +0700

    move register to login page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Menu from './Navbar'
-import LoginForm from '../LoginPage/LoginForm';
-import Register from './Register';
+import LoginForm from './LoginPage/LoginForm';
+import Register from './LoginPage/Register';
 import Profile from './Profile';
 import Volunteers from './Volunteers';
 import Profile2 from './Profile2';
-import ProtectedRoute from '../LoginPage/ProtectedRoute';
+import ProtectedRoute from './LoginPage/ProtectedRoute';
 const App = () => {
     return (
         <Router>
