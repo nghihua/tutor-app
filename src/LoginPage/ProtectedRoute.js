c32cd59609c040ae8fc4b3c5d8b2aed58b396d70
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2021-12-17 23:11:30 +0700

    quangae
    update  indent

--- src/LoginPage/ProtectedRoute.js
+++ src/LoginPage/ProtectedRoute.js
@@ -4,21 +4,21 @@ import Menu from '../components/Navbar';
 // Một hàm fetch data từ server về đây?
 
 const ProtectedRoute = ({ component: Component, ...rest }) => {
-    const isAuth = true;
-    return (
-        <div className="App">
-            <Menu />
-            <Route {...rest} render={(props) => {
-                if (isAuth) {
-                    return <Component />
-                }
-                else {
-                    return <Redirect to={{ pathname: '/', state: { from: props.location } }} />
-                }
-            }} />
-        </div>
+  const isAuth = true;
+  return (
+    <div className="App">
+      <Menu />
+      <Route {...rest} render={(props) => {
+        if (isAuth) {
+          return <Component />
+        }
+        else {
+          return <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+        }
+      }} />
+    </div>
 
-    );
+  );
 }
 
 export default ProtectedRoute;
