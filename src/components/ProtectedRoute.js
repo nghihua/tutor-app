a775c11c0f9c495ef64d15351310c08b6c6a34de
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2021-12-21 23:59:31 +0700

    add some change to React router

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,18 +1,23 @@
-import { Route, Navigate, useLocation } from 'react-router-dom'
+import { Navigate, useLocation } from 'react-router-dom'
 import React from 'react'
 import Menu from './Menu';
 // Một hàm fetch data từ server về đây?
 
-const ProtectedRoute = ({ component: Component}) => {
+const ProtectedRoute = ({ component: Component }) => {
   let location = useLocation();
   const isAuth = true;
-  
+
   if (isAuth) {
-    return <Component />
+    return (
+      <div className="user">
+        <Menu />
+        <Component />
+      </div>
+    )
   }
   else {
-    return <Navigate to='/' state= {{ from: location }} />
-  }  
+    return <Navigate to='/' state={{ from: location }} />
+  }
 }
 
 export default ProtectedRoute;
