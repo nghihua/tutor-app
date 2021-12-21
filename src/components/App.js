77c14b5546cb2ae971322339e2bcbce53613c370
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2021-12-21 21:54:44 +0700

    update Router version
    update to version 6

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Menu from './Navbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+// import Menu from './Menu'
 import LoginForm from './LoginForm';
 import Register from './Register';
 import Profile from './Profile';
@@ -10,17 +10,14 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route exact path="/">
-            <LoginForm />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-          <ProtectedRoute exact path="/volunteer" component={Volunteers} />
+        <Routes>
+          <Route exact path="/" element={<LoginForm />} />
+          <Route path="/register" element={<Register />} />
+            
+          {/* <ProtectedRoute exact path="/volunteer" component={Volunteers} />
           <ProtectedRoute exact path="/profile" component={Profile2} />
-          <ProtectedRoute exact path="/t" component={Profile} />
-        </Switch>
+          <ProtectedRoute exact path="/t" component={Profile} /> */}
+        </Routes>
       </div>
     </Router>
   );
