e2d8f5b8982e6c64e2f5eed00457f1510f4663fb
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2021-12-21 23:59:31 +0700

    add some change to React router

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Menu from './Menu'
 import LoginForm from './LoginForm';
 import Register from './Register';
 import Profile from './Profile';
 import Volunteers from './Volunteers';
 import Profile2 from './Profile2';
 import ProtectedRoute from './ProtectedRoute';
+
 const App = () => {
   return (
     <Router>
-      <Menu />
       <div className="App">
         <Routes>
           <Route exact path="/" element={<LoginForm />} />
           <Route path="/register" element={<Register />} />
-            
+
           <Route path="/volunteer" element={<ProtectedRoute component={Volunteers} />} />
-          {/* <ProtectedRoute path="/profile" component={Profile2} />
-          <ProtectedRoute path="/volunteer/profile" component={Profile} /> */}
+          <Route path="/profile/edit" element={<ProtectedRoute component={Profile2} />} />
+          <Route path="/profile" element={<ProtectedRoute component={Profile} />} />
+          {/* <Route path="/volunteer/:id" element={<ProtectedRoute component={VolunteerProfile} />} /> */}
+
         </Routes>
       </div>
     </Router>
