a89339ab2893500ce0394151420ac2987454540b
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2021-12-21 21:54:44 +0700

    update Router version
    update to version 6

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,24 +1,24 @@
-import { Route, Redirect } from 'react-router-dom'
-import React from 'react'
-import Menu from './Navbar';
-// Một hàm fetch data từ server về đây?
+// import { Route,  } from 'react-router-dom'
+// import React from 'react'
+// import Menu from './Navbar';
+// // Một hàm fetch data từ server về đây?
 
-const ProtectedRoute = ({ component: Component, ...rest }) => {
-  const isAuth = true;
-  return (
-    <div className="App">
-      <Menu />
-      <Route {...rest} render={(props) => {
-        if (isAuth) {
-          return <Component />
-        }
-        else {
-          return <Redirect to={{ pathname: '/', state: { from: props.location } }} />
-        }
-      }} />
-    </div>
+// const ProtectedRoute = ({ component: Component, ...rest }) => {
+//   const isAuth = true;
+//   return (
+//     <div className="App">
+//       <Menu />
+//       <Route {...rest} render={(props) => {
+//         if (isAuth) {
+//           return <Component />
+//         }
+//         else {
+//           return <Redirect to={{ pathname: '/', state: { from: props.location } }} />
+//         }
+//       }} />
+//     </div>
 
-  );
-}
+//   );
+// }
 
-export default ProtectedRoute;
+// export default ProtectedRoute;
