aa6d0283559ea3bf17a4a499c59492fb0ca7b102
TheFabulousP <anphu.hlap@gmail.com>
2021-12-22 17:06:57 +0700

    update template of "Log in"
    Add a navigation bar (menu)
    Edit color, format of "Log in" page according to Figma

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -12,32 +12,29 @@ const LoginForm = () => {
     history.push("/volunteer");
     //else => error
   };
-
+  
   return (
     <div>
       <div className='login'>
-        <form onSubmit={submitHandle}>
-          <h2>Login</h2>
-          {/* {(error) ? (<div className= "error"><h2>{error} </h2></div>) : ""} */}
+		<div className="leftcolumn">
+			<form onSubmit={submitHandle}>
+			  <h2>Welcome !</h2>
+			  {/* {(error) ? (<div className= "error"><h2>{error} </h2></div>) : ""} */}
 
-          <label htmlFor="email">Email: </label>
-          <input type="email" name="email" id="email" onChange={(event) => setDetails({ ...details, email: event.target.value })} value={details.email} />
+			  <input type="email" placeholder="Email" name="email" id="email" onChange={(event) => setDetails({ ...details, email: event.target.value })} value={details.email} />
 
-          <label htmlFor="password">Password: </label>
-          <input type="password" name="password" id="password" onChange={(event) => setDetails({ ...details, password: event.target.value })} value={details.password} />
+			  <input type="password" placeholder="Password" name="password" id="password" onChange={(event) => setDetails({ ...details, password: event.target.value })} value={details.password} />
 
-
-          <div className='center'>
-            <button type="submit" value="submit">Submit</button>
-          </div>
-        </form>
-        <div className='center'>
-          <Link to="/register">Register</Link>
-        </div>
+			  <button type="submit" value="submit">Submit</button>
+			  <div className="toregister">
+			  <Link to="/register">Register here</Link>
+			  </div>
+			</form>
+		</div>
+		<div className="rightcolumn">
+		{/*<img src={image.default} alt="image not found" />*/}
+		</div>
       </div>
-      <footer>
-        <p className="center">@tutor2021</p>
-      </footer>
     </div>
   );
 }
