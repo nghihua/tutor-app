0fd23fcbb8a4b886e6fd1797d6f0a0f3b1206c9e
TheFabulousP <anphu.hlap@gmail.com>
2021-12-26 15:21:06 +0700

    Edit CSS for Volunteer page

--- src/components/VolunteerList.js
+++ src/components/VolunteerList.js
@@ -3,30 +3,30 @@ const VolunteerList = ({ volunteers }) => {
     <ul className="volunteer-list center">
 
       {volunteers.map(({ email, fullName, major, intake }) => (
-        <li className="volunteer-preview" key={email} >
-          <div className="p-5 box" background-color="white">
-            <span className="box1">
-              <a href={`/${email}`}>
-                <img
-                  src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
-                  alt="profile pic"
-                  width="90"
-                  height="90"
-                  className="image"
-                />
-              </a>
-            </span>
-            <span className="box1">
-              <a href={`/${email}`}>
-                <div className="box2">
-                  <h2>{fullName}</h2>
-                  <p>{major} {intake}</p>
-                </div>
-              </a>
-            </span>
+<div className="volunteer-preview" key={email} >
+  <div className="volunteer-block" background-color="white">
+    <span className="box1">
+      <a href={"/${email}"}>
+        <img
+          src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+          alt="profile pic"
+          width="90"
+          height="90"
+          className="image rounded-circle"
+        />
+      </a>
+    </span>
+    <span className="volunteer-info">
+      <a href={"/${email}"}>
+        <div className="box2">
+          <h2>{fullName}</h2>
+          <p>{major} {intake}</p>
+        </div>
+      </a>
+    </span>
 
-          </div>
-        </li>
+  </div>
+</div>
       ))}
 
     </ul>
