7c61ae181f359b2b51f55c132975c8292f762dac
Kimthanh11 <18047@student.vgu.edu.vn>
2021-12-28 18:07:05 +0700

    Add the basic modal

--- src/components/ProfileEdit.js
+++ src/components/ProfileEdit.js
@@ -35,7 +35,7 @@ const ProfileEdit = ({ profile, onSave, onCancel }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="container">
+    <form onSubmit={handleSubmit} className="container edit">
       <div className="text-center pb-5 pt-5">
         <img
           src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
