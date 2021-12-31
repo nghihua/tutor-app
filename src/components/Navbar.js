6be208c06ab99eafa81c218155646da57d37f280
TheFabulousP <10421100@student.vgu.edu.vn>
2021-12-31 17:32:37 +0700

    Edit ProfileEdit and minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,20 +3,20 @@ import { Link } from 'react-router-dom'
 const Navbar = () => {
   return (
     <nav expand="lg" className="fixed-top b-5">
-      <img 
-        src="https://i.ibb.co/hDnvX38/logo-removebg-preview-removebg-preview.png"
-        alt="Tutor logo"
-        className="logo"
-      />
-      <div href="/" className="brand">Tutor</div>
+      <Link to="/">
+        <img 
+          src="https://i.ibb.co/hDnvX38/logo-removebg-preview-removebg-preview.png"
+          alt="Tutor logo"
+          className="logo"
+        />
+        <div className="brand">Tutor</div>
+      </Link>
       {/* try to figure how to connect Bootstrap and react router */}
       {/* <Nav.Link href="/Home">Home</Nav.Link>
                     <Nav.Link href="/EditProfile">Edit Profile</Nav.Link> */}
       {/* replace the Nav.Link with Link router */}
-      <div className="redirect">
-        <Link to="/">Home</Link>
-        <Link to="/profile">My Profile</Link>
-      </div>
+      <Link to="/profile" className="redirect">My Profile</Link>
+      <Link to="/" className="redirect">Home</Link>
     </nav>
 
   );
