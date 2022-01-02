ddc652b55df2e2e6b7b11f58bf24645564f300d6
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-01-02 17:25:06 +0700

    Fetch data for login

--- src/components/VolunteerList.js
+++ src/components/VolunteerList.js
@@ -5,30 +5,30 @@ const VolunteerList = ({ volunteers }) => {
     <ul className="volunteer-list center">
 
       {volunteers.map(({ email, fullName, major, intake }) => (
-<div className="volunteer-preview" key={email} >
-  <div className="volunteer-block" background-color="white">
-    <span className="box1">
-      <Link to={`/volunteer/${email}`}>
-        <img
-          src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
-          alt="profile pic"
-          width="90"
-          height="90"
-          className="image rounded-circle"
-        />
-      </Link>  
-    </span>
-    <span className="volunteer-info">
-      <Link to={`/volunteer/${email}`}>
-        <div className="box2">
-          <h2>{fullName}</h2>
-          <p>{major} {intake}</p>
-        </div>
-      </Link>
-    </span>
+        <div className="volunteer-preview" key={email} >
+          <div className="volunteer-block" background-color="white">
+            <span className="box1">
+              <Link to={`/volunteer/${email}`}>
+                <img
+                  src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                  alt="profile pic"
+                  width="90"
+                  height="90"
+                  className="image rounded-circle"
+                />
+              </Link>
+            </span>
+            <span className="volunteer-info">
+              <Link to={`/volunteer/${email}`}>
+                <div className="box2">
+                  <h2>{fullName}</h2>
+                  <p>{major} {intake}</p>
+                </div>
+              </Link>
+            </span>
 
-  </div>
-</div>
+          </div>
+        </div>
       ))}
 
     </ul >
