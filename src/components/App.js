0c8795c444778621f649248e026b3d58d79deb1c
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-01-02 17:25:06 +0700

    Fetch data for login

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Footer from './Footer';
-
 import LoginForm from './LoginForm';
 import Register from './Register';
 import Volunteers from './Volunteers';
 import Profile from './Profile';
 import ProtectedRoute from './ProtectedRoute';
 import Home from './Home';
+
 const App = () => {
   return (
     <Router>
@@ -17,7 +17,6 @@ const App = () => {
           <Route path="/register" element={<Register />} />
 
           <Route path="/volunteer" element={<ProtectedRoute component={Volunteers} />} />
-          <Route path="/profile/edit" element={<ProtectedRoute component={Profile2} />} />
           <Route path="/profile" element={<ProtectedRoute component={Profile} />} />
           {/* <Route path="/volunteer/:id" element={<ProtectedRoute component={VolunteerProfile} />} /> */}
 
