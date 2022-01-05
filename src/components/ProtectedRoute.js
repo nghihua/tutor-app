2761ba3f0371962f5d5230046e2dff63393c6797
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-01-05 20:13:08 +0700

    useFetch
    add useFetch add some console.log in server

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,16 +1,19 @@
 import { Navigate, useLocation } from 'react-router-dom'
 import { React } from 'react'
 import Menu from './Menu';
+import useFetch2 from './useFetch2'
 // Một hàm fetch data từ server về đây?
 
 const ProtectedRoute = ({ component: Component }) => {
   let location = useLocation();
-  let isAuth = true;
   // const Logout =() =>{
   //   setIsAuth(true);
   // }
-  if (isAuth) {
-    console.log(isAuth);
+  const url = 'http://localhost:5000/api/auth/login_status';
+
+  const {data: auth, error} = useFetch2(url);
+  if (auth) {
+    console.log(auth);
     return (
       <div className="user">
         <Menu />
@@ -19,7 +22,7 @@ const ProtectedRoute = ({ component: Component }) => {
     )
   }
   else {
-    console.log(isAuth);
+    console.log(auth);
     alert("You must login to see the page")
     return <Navigate to='/login' state={{ from: location }} />
   }
