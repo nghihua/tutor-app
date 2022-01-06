9b3f2e1fce2db0f3ea64b3c55c2dd66dd929f62a
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-01-06 21:43:08 +0700

    Add useConst hook, update Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,47 +1,56 @@
 import { useState } from "react";
 import ProfileEdit from "./ProfileEdit";
 import ProfileView from "./ProfileView";
+import { useFetch, useConst } from "../hooks/custom-hooks";
 
-const Profile = () => {
-  const [profile, setProfile] = useState({
-    "email": "xxxx1@student.vgu.edu.vn",
-    "fullName": "Nguyen Van A",
-    "major": "CSE",
-    "intake": "2019",
-    "isTutor": true,
-    "subjects": [
-      "Math",
-      "C Programming"
-    ],
-  });
+const Profile = ({ id }) => {
+  // const [profile, setProfile] = useState({
+  //   email: "xxxx1@student.vgu.edu.vn",
+  //   fullName: "Nguyen Van A",
+  //   major: "CSE",
+  //   intake: "2019",
+  //   isTutor: true,
+  //   subjects: ["Math", "C Programming"],
+  // });
+  const requestInit = useConst({ credentials: "include" });
+  const options = { asEffect: true };
 
-  const isOwner = true; // performs check on whether the profile belongs to the current logged in account
+  const {
+    data: user,
+    error,
+    isLoading,
+  } = useFetch(`http://localhost:5000/api/user/${id}`, requestInit, options);
+
+  const { data: canEdit } = useFetch(
+    `http://localhost:5000/api/user/${id}/edit_permission`,
+    requestInit,
+    options
+  );
   const [isEditing, setIsEditing] = useState(false);
 
-  const handleSave = newProfile => {
+  const handleSave = (newProfile) => {
     console.log(JSON.stringify(newProfile));
-    setProfile(newProfile);
+    // setProfile(newProfile);
     setIsEditing(false);
   };
 
   return (
     <div className="profile">
-      {
-        isEditing ?
-          <ProfileEdit
-            profile={profile}
-            onSave={handleSave}
-            onCancel={() => setIsEditing(false)}
-          /> :
-          <ProfileView
-            profile={profile}
-            isOwner={isOwner}
-            onEdit={() => setIsEditing(true)}
-          />
-      }
-
+      {isEditing ? (
+        <ProfileEdit
+          user={user}
+          onSave={handleSave}
+          onCancel={() => setIsEditing(false)}
+        />
+      ) : (
+        <ProfileView
+          user={user}
+          isOwner={canEdit}
+          onEdit={() => setIsEditing(true)}
+        />
+      )}
     </div>
   );
-}
+};
 
 export default Profile;
