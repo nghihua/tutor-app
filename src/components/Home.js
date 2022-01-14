14817de69e1e55ae933526a6b4801808a5d8b25e
TheFabulousP <10421100@student.vgu.edu.vn>
2022-01-14 18:40:17 +0700

    Update outline of Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,40 @@ import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div className="home">
-      <h2>This is a Home Page</h2>
-      <Link to="/volunteers">Go to Logasdasdin</Link>
-      <p> </p>
-    </div>
+<div className="home">
+    
+  <header>
+    <h1>TUTOR HOMEPAGE</h1>
+  </header>
+    
+  <div className="Welcome">
+    <h2>Welcome to Tutor</h2>
+    <Link to="/login">
+      <button>Sign in</button>
+    </Link>
+    <Link to="/register">
+      <button>Sign up</button>
+    </Link>
+  </div>
+    
+  <div className="About-us">
+    <h3>Who are we?</h3>
+    <h2>ABOUT US</h2>
+  </div>
+    
+  <div className="Privilege">
+  </div>
+    
+  <div className="Subject">
+    <h2>SUBJECT</h2>
+  </div>
+    
+  <div className="Quote">
+    <img alt="image" src="homeimg2.png" />
+    <p>Tell me and I forget, teach me and I may remember, involve me and I learn</p>
+    <h4>Benjamin Franklin</h4>
+  </div>
+</div>
   );
 }
 
