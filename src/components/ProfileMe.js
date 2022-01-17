70c3cd651fff9ecff6fe04a42b02ba09986a4de6
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-01-17 21:26:54 +0700

    Update ProfileMe to useAuth

--- src/components/ProfileMe.js
+++ src/components/ProfileMe.js
@@ -1,31 +1,9 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { useFetch } from "../hooks/custom-hooks";
-import { includeCredentials } from "../utils";
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../hooks/custom-hooks";
 
 const ProfileMe = () => {
-  const navigate = useNavigate();
-  const { doFetch: fetchCurrentUser } = useFetch(
-    "http://localhost:5000/api/user/current",
-    includeCredentials,
-    { usingState: false }
-  );
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    fetchCurrentUser()
-      .then((user) => {
-        navigate(`/profile/${user.user_id}`, { replace: true });
-      })
-      .catch((err) => {
-        if (err.name !== "AbortError") {
-          setError("An error has occurred.");
-          console.error(err);
-        }
-      });
-  }, [fetchCurrentUser, navigate]);
-
-  return <h3>{error || "Loading..."}</h3>;
+  const auth = useAuth();
+  return <Navigate to={`/profile/${auth.user.user_id}`} replace />;
 };
 
 export default ProfileMe;
