962c2ec58cef94c817d4dcbf1d79b4c2dfb7164b
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-01-17 21:03:39 +0700

    Updating routing structure added use of Outlet ProtectedRoute moved to a single <Route/> wrapping other routes that require auth Navbar also moved to a single route Update auth context to provide login and logout functions Update LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,56 +1,82 @@
-import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-const LoginForm = () => {
-  const [details, setDetails] = useState({email: "", password: "" });
-  // const [error, setError] = useState(false) // set something if login not correct
-  let navigate = useNavigate(); // navigate to the volunteer page
+import { useState } from "react";
+import { Link, Navigate, useLocation, useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/custom-hooks";
+import { getFetchErrMsg } from "../utils";
 
-  const url = 'http://localhost:5000/api/auth/login';
-  const postData = async (url, data = {}) => {
-    const response = await fetch(url, {
-      method: 'POST',
-      credentials: "include",
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    return response.json();
-  }
-  const submitHandle = (event) => {
+const LoginForm = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const auth = useAuth();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [error, setError] = useState(null); // set error if login not correct
+
+  // after login, navigate to where the user came from or to the index page by default
+  const from = location.state?.from ?? "/";
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    postData(url, details)
-    .then(result => {
-      console.log(result)
-      navigate("/volunteers");
-    })
-    .catch(error => console.log(error.response));
+    setIsSubmitting(true);
+
+    auth.logIn(
+      { email, password },
+      () => {
+        navigate(from, { replace: true });
+      },
+      (err) => {
+        setError(getFetchErrMsg(err.message));
+        setIsSubmitting(false);
+      }
+    );
   };
 
+  if (auth.isLoggedIn === null) {
+    return <h3>Loading...</h3>;
+  }
+
+  if (auth.isLoggedIn) {
+    return <Navigate to={from} replace />;
+  }
+
   return (
-    <div>
-      <div className='login'>
-        <div >
-          <form onSubmit={submitHandle}>
-            <h2>Welcome !</h2>
-            {/* {(error) ? (<div className= "error"><h2>{error} </h2></div>) : ""} */}
+    <div className="login">
+      <form onSubmit={handleSubmit}>
+        <h2>Welcome!</h2>
 
-            <input type="email" placeholder="Email" name="email" id="email" onChange={(event) => setDetails({ ...details, email: event.target.value })} value={details.email} />
+        {error && <h4 style={{ color: "red" }}>{error}</h4>}
 
-            <input type="password" placeholder="Password" name="password" id="password" onChange={(event) => setDetails({ ...details, password: event.target.value })} value={details.password} />
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          id="email"
+          onChange={(event) => setEmail(event.target.value)}
+          value={email}
+        />
 
-            <button type="submit" value="submit">Submit</button>
-            <div className="toregister">
-              <Link to="/register">Register here</Link>
-            </div>
-          </form>
+        <input
+          type="password"
+          placeholder="Password"
+          name="password"
+          id="password"
+          onChange={(event) => setPassword(event.target.value)}
+          value={password}
+        />
+
+        <button type="submit" value="submit" disabled={isSubmitting}>
+          Submit
+        </button>
+
+        <div className="toregister">
+          <Link to="/register" state={location.state}>
+            Register here
+          </Link>
         </div>
-        <div >
-          {/*<img src={image.default} alt="image not found" />*/}
-        </div>
-      </div>
+      </form>
     </div>
   );
-}
+};
 
 export default LoginForm;
