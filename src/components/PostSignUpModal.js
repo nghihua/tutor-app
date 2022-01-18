292cfdd713b6ec79485b6ae0d5c25c6abda9ddda
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-01-18 13:10:36 +0700

    Extract the modal after signup to a separate component

---
+++ src/components/PostSignUpModal.js
@@ -0,0 +1,48 @@
+import { useEffect, useRef } from "react";
+import { useAuth } from "../hooks/custom-hooks";
+import ProfileEdit from "./ProfileEdit";
+
+const PostSignUpModal = () => {
+  const auth = useAuth();
+  const btnRef = useRef();
+
+  useEffect(() => {
+    btnRef.current.click();
+  }, []);
+
+  return (
+    <>
+      <button
+        type="button"
+        ref={btnRef}
+        className="btn btn-primary invisible"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Do you want to be a volunteer?
+      </button>
+
+      <div
+        className="modal fade"
+        id="exampleModal"
+        data-keyboard="false"
+        data-backdrop="static"
+      >
+        <div className="modal-dialog modal-lg modal-dialog-scrollable">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">
+                Do you want to be a volunteer?
+              </h5>
+            </div>
+            <div className="modal-body">
+              <ProfileEdit user={auth.user} onCancel={() => {}} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PostSignUpModal;
