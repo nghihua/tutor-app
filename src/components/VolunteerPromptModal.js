b4052377bb55ba1afcbe52b51dbce7bc3806b865
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-01-19 19:59:56 +0700

    Update Modal to have fade transition when switching content

--- src/components/VolunteerPromptModal.js
+++ src/components/VolunteerPromptModal.js
@@ -3,29 +3,55 @@ import { useEffect, useRef, useState } from "react";
 import { useAuth } from "../hooks/custom-hooks";
 import ProfileEdit from "./ProfileEdit";
 
+// modal's content enum constants
+const PROMPT = "PROMPT";
+const EDIT = "EDIT";
+
 const VolunteerPromptModal = ({ onClose }) => {
   const auth = useAuth();
   const modalRef = useRef();
-  const [isEditing, setIsEditing] = useState(false);
-  const onCloseRef = useRef(onClose);
 
+  const [content, setContent] = useState(PROMPT);
+  const isSwitchingContentRef = useRef(false);
+
+  const onCloseRef = useRef(onClose);
   useEffect(() => {
     onCloseRef.current = onClose;
   }, [onClose]);
 
   // setup and show modal
   useEffect(() => {
+    const modal = new Modal(modalRef.current);
+
     modalRef.current.addEventListener("hidden.bs.modal", () => {
-      onCloseRef.current?.();
+      if (!isSwitchingContentRef.current) {
+        onCloseRef.current?.(); // raise close event
+      } else {
+        // switch content
+        setContent(EDIT);
+        modal.show();
+        isSwitchingContentRef.current = false; // disable switching content
+      }
     });
 
-    const modal = new Modal(modalRef.current);
     modal.show();
   }, []);
 
+  // handle prompt agree
+  const switchContent = () => {
+    isSwitchingContentRef.current = true; // enable switching content
+    closeModal(); // the remaining actions are in the hidden.bs.modal event handler
+  };
+
   // profile edit callbacks
   const closeModal = () => {
-    Modal.getInstance(modalRef.current).hide();
+    const modal = Modal.getInstance(modalRef.current);
+    modal.hide();
+  };
+
+  const handleSaveSuccess = () => {
+    alert("Saved successfully!");
+    closeModal();
   };
 
   const handleSaveError = (err) => {
@@ -33,15 +59,42 @@ const VolunteerPromptModal = ({ onClose }) => {
     alert("Unable to save. An error has occurred.");
   };
 
+  // modal's content
+  const { title, body } = {
+    [PROMPT]: {
+      title: "Do you want to be a volunteer?",
+      body: (
+        <>
+          <button type="button" onClick={switchContent}>
+            Yes
+          </button>
+          <button type="button" data-bs-dismiss="modal">
+            No
+          </button>
+        </>
+      ),
+    },
+    [EDIT]: {
+      title: "Please fill in your tutor information",
+      body: (
+        <ProfileEdit
+          user={auth.user}
+          checkIsTutor
+          onSaveSuccess={handleSaveSuccess}
+          onSaveError={handleSaveError}
+          onCancel={closeModal}
+        />
+      ),
+    },
+  }[content];
+
   return (
     <div className="modal fade" id="volunteerPromptModal" ref={modalRef}>
       <div className="modal-dialog modal-lg modal-dialog-scrollable">
         <div className="modal-content">
           <div className="modal-header">
             <h5 className="modal-title" id="volunteerPromptModalLabel">
-              {isEditing
-                ? "Please fill in your tutor information"
-                : "Do you want to be a volunteer?"}
+              {title}
             </h5>
 
             <button
@@ -49,33 +102,10 @@ const VolunteerPromptModal = ({ onClose }) => {
               className="btn-close"
               data-bs-dismiss="modal"
               aria-label="Close"
-            >
-              {/* Cái button này ở trên góc phải cũng để close.
-              Có gì Phú hoặc ai chỉnh css lại giùm t thành dấu 'x' này nọ nha.
-              Thanks */}
-            </button>
+            ></button>
           </div>
 
-          <div className="modal-body">
-            {isEditing ? (
-              <ProfileEdit
-                user={auth.user}
-                checkIsTutor
-                onSaveSuccess={closeModal}
-                onSaveError={handleSaveError}
-                onCancel={closeModal}
-              />
-            ) : (
-              <>
-                <button type="button" onClick={() => setIsEditing(true)}>
-                  Yes
-                </button>
-                <button type="button" data-bs-dismiss="modal">
-                  No
-                </button>
-              </>
-            )}
-          </div>
+          <div className="modal-body">{body}</div>
         </div>
       </div>
     </div>
