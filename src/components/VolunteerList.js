27d3ed7b4d7be5cd4fdfca5ed0d089b225db9980
Kimthanh11 <18047@student.vgu.edu.vn>
2022-01-24 22:45:56 +0700

    Update pagination using library

--- src/components/VolunteerList.js
+++ src/components/VolunteerList.js
@@ -1,32 +1,63 @@
-import { useSearchParams } from "react-router-dom";
-import Pagination from "./Pagination";
 import VolunteerPreview from "./VolunteerPreview";
+import ReactPaginate from "react-paginate";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const volunteersPerPage = 3;
 
 const VolunteerList = ({ volunteers }) => {
-  // Get current page from url search parameter
-  const [search] = useSearchParams();
-  const pageParam = parseInt(search.get("page")) || 1; // default to 1 if parsed to NaN
-  const currentPage = Math.max(pageParam, 1); // default to 1 if negative
 
-  // Get current volunteers
-  const lastIndex = currentPage * volunteersPerPage;
-  const firstIndex = lastIndex - volunteersPerPage;
-  const currentVolunteers = volunteers.slice(firstIndex, lastIndex);
+  const [cvolunteers, setCvolunteers] = useState([]);
+
+
+  const fetchVolunteers = (currentPage) => {
+    // Get current volunteers
+    const lastIndex = currentPage * volunteersPerPage;
+    const firstIndex = lastIndex - volunteersPerPage;
+    const currentVolunteers = volunteers.slice(firstIndex, lastIndex);
+    return currentVolunteers;
+  }
+
+  useEffect(() => {
+    const volunteersFormServer =  fetchVolunteers(1);
+    setCvolunteers(volunteersFormServer);
+  }, []);
+
+
+  const handlePageClick = (data) => {
+    let currentPage = data.selected + 1;
+
+    const volunteersFormServer =  fetchVolunteers(currentPage);
+    setCvolunteers(volunteersFormServer);
+  }
 
   return (
     <div className="volunteer-list">
       <div className="center">
-        {currentVolunteers.map((user) => (
+        {cvolunteers.map((user) => (
           <VolunteerPreview user={user} key={user.user_id} />
         ))}
       </div>
 
-      <Pagination
-        postPerPage={volunteersPerPage}
-        totalPosts={volunteers.length}
-      />
+<ReactPaginate
+        previousLabel={'previous'}
+        nextLabel={'next'}
+        breakLabel={'...'}
+        pageCount={4}
+        marginPagesDisplayed={2}
+        pageRangeDisplayed={3}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination justify-content-center"}
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        previousClassName="page-item"
+        previousLinkClassName="page-link"
+        nextClassName="page-item"
+        nextLinkClassName="page-link"
+        breakClassName="page-item"
+        breakLinkClassName="page-link"
+        activeClassName="active"
+        />
     </div>
   );
 };
