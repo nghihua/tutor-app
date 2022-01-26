4bb7b8088ff955c5bee5457c1fa8f9d6965e5120
WataNekko <88185666+WataNekko@users.noreply.github.com>
2022-01-26 13:00:44 +0700

    Restructure Modal components extract show modal logic to ModalManager extract modal base component to ModalComponent put all modal related components to submodule "modals"

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link, Outlet, useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../hooks/custom-hooks";
-// import TestModalBtn from "./TestModalBtn";
+// import { TestModalBtn } from "components";
 
 const Navbar = () => {
   const auth = useAuth();
